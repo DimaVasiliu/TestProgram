55461afec16255a980c5fb9333204a79cdcadc0d
Dima <dima.vasiliu@yahoo.com>
2023-06-23 09:51:30 +0100

    first commit

---
+++ test2.java
@@ -0,0 +1,17 @@
+public static test2 {
+    public static void main (String args[]) {
+    int time = 22;
+    if (time < 10)
+{
+    System.out.println("Good Morning");
+}
+else if (time <18)
+{
+    System.out.println("Good day");
+}
+else
+{
+    System.out.println("Good evening");
+}
+}
+}
